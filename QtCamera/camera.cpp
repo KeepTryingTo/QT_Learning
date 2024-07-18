3aee5058648fdb49837e552b697f8721a57984aa
KeepTryingTo <96606723+KeepTryingTo@users.noreply.github.com>
2024-07-18 10:09:25 +0800

    Add files via upload

---
+++ QtCamera/camera.cpp
@@ -0,0 +1,79 @@
+﻿#include "camera.h"
+#include "ui_camera.h"
+#include <QCameraDevice>
+#include <QMediaDevices>
+#include <QComboBox>
+#include <QVideoFrame>
+#include <QSize>
+
+
+Camera::Camera(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Camera)
+{
+    ui->setupUi(this);
+
+    getCameras();
+
+    this -> m_camera = new QCamera(QMediaDevices::defaultVideoInput());
+    this -> mediaCaptureSession = new QMediaCaptureSession();
+
+    connect(ui -> comboBox,&QComboBox::currentIndexChanged,this,&Camera::selectCam);
+}
+
+Camera::~Camera(){
+    delete ui;
+}
+
+void Camera::getCameras(){
+    //默认设备为None
+    ui -> comboBox -> addItem("<None>");
+    //首先遍历所有的可用设备
+    this -> availCameraDevices = QMediaDevices::videoInputs();
+    for(const QCameraDevice & camera : availCameraDevices){
+        ui -> comboBox -> addItem(camera.description());
+        // qDebug()<<"current available device: "<<camera.description();
+    }
+}
+//退出当前程序
+void Camera::on_btn_close_clicked(){
+    this -> close();
+}
+//打开摄像头
+void Camera::on_btn_start_clicked(){
+    this -> m_camera -> start();
+}
+
+//关闭摄像头
+void Camera::on_btn_stop_clicked(){
+    if(this -> m_camera->isActive()){
+        this -> m_camera->stop();
+        disconnect(sink);
+    }
+}
+
+
+void Camera::selectCam(int index){
+    //遍历当前选择的设备是否等于可利用的设备，然后进行设备的配置
+    for(const QCameraDevice &camera : availCameraDevices){
+        if(camera.description() == ui -> comboBox->currentText()){
+            this -> m_camera -> setCameraDevice(camera);
+            this -> mediaCaptureSession -> setCamera(this -> m_camera);
+            this -> mediaCaptureSession -> setVideoOutput(ui -> videowidget);
+            qDebug()<<"current select camera: "<<camera.description();
+
+            //关于sink的作用，里面包含了关于帧的一些基本信息
+            sink = this -> mediaCaptureSession -> videoSink();
+            connect(sink,&QVideoSink::videoFrameChanged,this,&Camera::onFrameChanged);
+
+            break;
+        }
+    }
+}
+
+void Camera::onFrameChanged(const QVideoFrame&frame){
+    QSize size = frame.size();
+    qDebug()<<"frame size: "<<size;
+    ui -> lab_resl -> setText(QString("%1 x %2").arg(QString::number(size.width()),QString::number(size.height())));
+}
+
---
+++ QtCamera/camera.h
@@ -0,0 +1,57 @@
+﻿#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <QMainWindow>
+#include <QtWidgets>
+#include <QtMultimedia>
+#include <QtMultimediaWidgets>
+#include <QImageCapture>
+#include <QCameraDevice>
+#include <QMediaDevices>
+#include <QList>
+#include <QMediaCaptureSession>
+#include <QVideoWidget>
+#include <QVideoSink>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Camera;
+}
+QT_END_NAMESPACE
+
+class Camera : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Camera(QWidget *parent = nullptr);
+    ~Camera();
+
+    void getCameras();
+    void onFrameChanged(const QVideoFrame&frame);
+    
+private slots:
+    
+    void on_btn_close_clicked();
+    
+    void on_btn_start_clicked();
+
+    void on_btn_stop_clicked();
+
+    void selectCam(int index);
+
+private:
+    Ui::Camera *ui;
+
+    QCamera *m_camera;
+    QVideoSink * sink;
+
+    QScopedPointer<QImageCapture> m_camera_capture;
+    QList<QCameraDevice> availCameraDevices;
+    //QMediaCaptureSession： https://blog.csdn.net/yegu001/article/details/135767425
+    QMediaCaptureSession * mediaCaptureSession;
+
+    int Counter = 0;
+    QImage imageFromCamera;
+};
+#endif // CAMERA_H
