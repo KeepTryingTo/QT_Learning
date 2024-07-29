461398b09fc56d1ed5a50c8c32475f58025a445d
KeepTryingTo <96606723+KeepTryingTo@users.noreply.github.com>
2024-07-29 10:44:36 +0800

    Add files via upload

---
+++ QtNetworkAccessManager/networkaccessmanagers.cpp
@@ -0,0 +1,50 @@
+﻿#include "networkaccessmanagers.h"
+#include "ui_networkaccessmanagers.h"
+
+#include <QFile>
+#include <QUrl>
+#include <QMessageBox>
+
+NetworkAccessManagers::NetworkAccessManagers(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::NetworkAccessManagers)
+{
+    ui->setupUi(this);
+
+    this -> Nmanager = new QNetworkAccessManager(this);
+}
+
+NetworkAccessManagers::~NetworkAccessManagers()
+{
+    delete ui;
+}
+
+void NetworkAccessManagers::on_btn_get_clicked()
+{
+    QString api = ui -> lineEdit -> text();//https://api.country.is/9.9.9.9
+    QUrl APIurl(api);
+    QNetworkRequest request(APIurl);
+
+    this -> reply = this -> Nmanager -> get(request);
+
+    QEventLoop loop;
+    connect(reply,&QNetworkReply::finished,&loop,&QEventLoop::quit);
+    loop.exec();
+
+    if(reply -> error() == QNetworkReply::NoError){
+        QString response = reply -> readAll();
+        qDebug()<<"API Response: "<<response;
+        ui -> plainTextEdit -> appendPlainText("API Response: " + response);
+    }else{
+        qDebug()<<"Error: "<<reply -> errorString();
+        ui -> plainTextEdit -> appendPlainText("Error: " + reply -> errorString());
+    }
+    reply -> deleteLater();
+}
+
+
+void NetworkAccessManagers::on_btn_close_clicked()
+{
+    this -> close();
+}
+
---
+++ QtNetworkAccessManager/networkaccessmanagers.h
@@ -0,0 +1,36 @@
+﻿#ifndef NETWORKACCESSMANAGERS_H
+#define NETWORKACCESSMANAGERS_H
+
+#include <QMainWindow>
+
+#include <QNetworkAccessManager>
+#include <QNetworkAddressEntry>
+#include <QNetworkReply>
+#include <QDesktopServices>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class NetworkAccessManagers;
+}
+QT_END_NAMESPACE
+
+class NetworkAccessManagers : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    NetworkAccessManagers(QWidget *parent = nullptr);
+    ~NetworkAccessManagers();
+
+private slots:
+    void on_btn_get_clicked();
+
+    void on_btn_close_clicked();
+
+private:
+    Ui::NetworkAccessManagers *ui;
+
+    QNetworkAccessManager * Nmanager;
+    QNetworkReply * reply;
+};
+#endif // NETWORKACCESSMANAGERS_H
