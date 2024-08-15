d4738bff54b12f40413429e1f812310ea4dd7e7e
KeepTryingTo <96606723+KeepTryingTo@users.noreply.github.com>
2024-08-15 16:48:02 +0800

    Add files via upload

---
+++ QtOpenCV/QtOpenCV_readNetFromONNX_v2/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "opencvdnn.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    OpenCVDNN w;
+    w.show();
+    return a.exec();
+}
---
+++ QtOpenCV/QtOpenCV_readNetFromONNX_v2/opencvdnn.h
@@ -0,0 +1,60 @@
+﻿#ifndef OPENCVDNN_H
+#define OPENCVDNN_H
+
+#include <QMainWindow>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/video/video.hpp>
+
+#include <QFile>
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QMessageBox>
+#include <QSize>
+#include <QPixmap>
+#include <QTime>
+#include <QDateTime>
+#include <QString>
+#include <QMap>
+#include <QVector>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class OpenCVDNN;
+}
+QT_END_NAMESPACE
+
+class OpenCVDNN : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    OpenCVDNN(QWidget *parent = nullptr);
+    ~OpenCVDNN();
+
+    QImage cvMatToQImage(const cv::Mat &inMat);
+    cv::Mat normalizeBlob(cv::Mat& inputBlob, cv::Scalar & mean, cv::Scalar & std);
+
+private slots:
+    void on_btn_open_file_clicked();
+
+    void on_btn_predict_clicked();
+
+    void on_btn_close_clicked();
+
+    void on_btn_open_model_clicked();
+
+private:
+    Ui::OpenCVDNN *ui;
+
+    cv::Mat Image;
+    QString modelPath;
+    QMap<int,QString>indexMapName;
+    cv::dnn::Net model;
+};
+#endif // OPENCVDNN_H
