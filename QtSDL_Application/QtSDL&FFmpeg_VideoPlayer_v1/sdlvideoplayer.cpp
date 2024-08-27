7b00dd571f53a7a50659a4521472b06ac7faca73
KeepTryingTo <96606723+KeepTryingTo@users.noreply.github.com>
2024-08-28 07:49:41 +0800

    Add files via upload

---
+++ QtSDL_Application/QtSDL&FFmpeg_VideoPlayer_v1/sdlvideoplayer.cpp
@@ -0,0 +1,16 @@
+﻿#include "sdlvideoplayer.h"
+#include "ui_sdlvideoplayer.h"
+
+SDLVideoPlayer::SDLVideoPlayer(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::SDLVideoPlayer)
+{
+    ui->setupUi(this);
+}
+
+SDLVideoPlayer::~SDLVideoPlayer()
+{
+    delete ui;
+}
+
+
---
+++ QtSDL_Application/QtSDL&FFmpeg_VideoPlayer_v1/sdlvideoplayer.h
@@ -0,0 +1,23 @@
+﻿#ifndef SDLVIDEOPLAYER_H
+#define SDLVIDEOPLAYER_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class SDLVideoPlayer;
+}
+QT_END_NAMESPACE
+
+class SDLVideoPlayer : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    SDLVideoPlayer(QWidget *parent = nullptr);
+    ~SDLVideoPlayer();
+
+private:
+    Ui::SDLVideoPlayer *ui;
+};
+#endif // SDLVIDEOPLAYER_H
